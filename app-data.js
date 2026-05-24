8d1e78b324a12af1c3c63fc4f32d107eb601c730
Jzzhou <joezhow777@gmail.com>
2026-05-24 23:26:59 +0800

    Add site data

---
+++ app-data.js
@@ -0,0 +1,80 @@
+window.FINANCE_KING_DATA = {
+  "source": {
+    "type": "excel",
+    "name": "第一届“谁是理财王”全球理财挑战赛 赛程文档.xlsx",
+    "updatedAt": "2026-05-24T22:55:08"
+  },
+  "players": [
+    {
+      "id": "大哥",
+      "name": "大哥",
+      "initial": 100000,
+      "current": 100000,
+      "cleanStart": true,
+      "hasReport": false,
+      "strategy": "",
+      "profile": ""
+    },
+    {
+      "id": "大爷",
+      "name": "大爷",
+      "initial": 100000,
+      "current": 100000,
+      "cleanStart": true,
+      "hasReport": false,
+      "strategy": "",
+      "profile": "基金经理介绍：拥有多商品、多市场、多时区的丰富韭菜经验，A股扛过跌、B圈爆过仓、美股套过牢。擅长刷社交媒体找投资灵感，有微博、雪球、知乎、Reddit、X等多种内部消息渠道。投资纪律严谨，值得托付：灵光一现、脑门一拍、双脚一跺、两手一摊、倒头就睡、明天再说。"
+    },
+    {
+      "id": "师兄",
+      "name": "师兄",
+      "initial": 100000,
+      "current": 100000,
+      "cleanStart": true,
+      "hasReport": false,
+      "strategy": "",
+      "profile": ""
+    },
+    {
+      "id": "鱿鱼",
+      "name": "鱿鱼",
+      "initial": 100000,
+      "current": 100000,
+      "cleanStart": true,
+      "hasReport": false,
+      "strategy": "",
+      "profile": ""
+    },
+    {
+      "id": "老齐",
+      "name": "老齐",
+      "initial": 100000,
+      "current": 100000,
+      "cleanStart": true,
+      "hasReport": false,
+      "strategy": "",
+      "profile": ""
+    },
+    {
+      "id": "周鸟",
+      "name": "周鸟",
+      "initial": 100000,
+      "current": 100000,
+      "cleanStart": true,
+      "hasReport": false,
+      "strategy": "",
+      "profile": ""
+    },
+    {
+      "id": "男模",
+      "name": "男模",
+      "initial": 100000,
+      "current": 100000,
+      "cleanStart": true,
+      "hasReport": false,
+      "strategy": "",
+      "profile": ""
+    }
+  ],
+  "trades": []
+};
